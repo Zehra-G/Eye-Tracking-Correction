7c9d3e067c08c8bf144313bb2a96a248815e7a4c
zgundo25@colby.edu <zehragundogdu@Zehras-MacBook-Air.local>
2023-04-09 21:49:25 -0400

    added 2 diff versions of compare sanches paper

--- Algorithms/drift_algorithms.py
+++ Algorithms/drift_algorithms.py
@@ -131,6 +131,91 @@ def compare(fixation_XY, word_XY, x_thresh=512, n_nearest_lines=3):
 	return fixation_XY
 
 ######################################################################
+# COMPARE BY FIXATION NUMBER
+#
+# Lima Sanches, C., Kise, K., & Augereau, O. (2015). Eye gaze and text
+#   line matching for reading analysis. In Adjunct proceedings of the
+#   2015 ACM International Joint Conference on Pervasive and
+#   Ubiquitous Computing and proceedings of the 2015 ACM International
+#   Symposium on Wearable Computers (pp. 1227–1233). Association for
+#   Computing Machinery.
+#
+# https://doi.org/10.1145/2800835.2807936
+######################################################################
+
+#This function implements the same fixation detection and line break detection, however assigns fixation
+# lines to text lines by comparing the fixation numbers rather than DTW
+
+def compare_fixnum(fixation_XY, word_XY, x_thresh=512, n_nearest_lines=3):
+    line_Y = np.unique(word_XY[:, 1])
+    n = len(fixation_XY)
+    diff_X = np.diff(fixation_XY[:, 0])
+    end_line_indices = list(np.where(diff_X < -x_thresh)[0] + 1)
+    end_line_indices.append(n)
+    start_of_line = 0
+    for end_of_line in end_line_indices:
+        gaze_line = fixation_XY[start_of_line:end_of_line]
+        mean_y = np.mean(gaze_line[:, 1])
+        lines_ordered_by_proximity = np.argsort(abs(line_Y - mean_y))
+        nearest_line_I = lines_ordered_by_proximity[:n_nearest_lines]
+        line_scores = np.zeros(n_nearest_lines)
+        for candidate_i in range(n_nearest_lines):
+            candidate_line_i = nearest_line_I[candidate_i]
+            text_line = word_XY[word_XY[:, 1] == line_Y[candidate_line_i]]
+            Nf = len(gaze_line)
+            Nw = len(text_line)
+            if Nf == Nw:
+                line_scores[candidate_i] = 0
+            else:
+                line_scores[candidate_i] = abs(Nf - Nw)
+        line_i = nearest_line_I[np.argmin(line_scores)]
+        fixation_XY[start_of_line:end_of_line, 1] = line_Y[line_i]
+        start_of_line = end_of_line
+    return fixation_XY
+
+
+######################################################################
+# COMPARE BY LINE LENGTH
+#
+# Lima Sanches, C., Kise, K., & Augereau, O. (2015). Eye gaze and text
+#   line matching for reading analysis. In Adjunct proceedings of the
+#   2015 ACM International Joint Conference on Pervasive and
+#   Ubiquitous Computing and proceedings of the 2015 ACM International
+#   Symposium on Wearable Computers (pp. 1227–1233). Association for
+#   Computing Machinery.
+#
+# https://doi.org/10.1145/2800835.2807936
+######################################################################
+
+#This function implements the same fixation detection and line break detection, however assigns fixation
+# lines to text lines by comparing the line length rather than DTW
+
+def compare_line_len(fixation_XY, word_XY, x_thresh=512, n_nearest_lines=3):
+    line_Y = np.unique(word_XY[:, 1])
+    n = len(fixation_XY)
+    diff_X = np.diff(fixation_XY[:, 0])
+    end_line_indices = list(np.where(diff_X < -x_thresh)[0] + 1)
+    end_line_indices.append(n)
+    start_of_line = 0
+    for end_of_line in end_line_indices:
+        gaze_line = fixation_XY[start_of_line:end_of_line]
+        mean_y = np.mean(gaze_line[:, 1])
+        lines_ordered_by_proximity = np.argsort(abs(line_Y - mean_y))
+        nearest_line_I = lines_ordered_by_proximity[:n_nearest_lines]
+        line_scores = np.zeros(n_nearest_lines)
+        for candidate_i in range(n_nearest_lines):
+            candidate_line_i = nearest_line_I[candidate_i]
+            text_line = word_XY[word_XY[:, 1] == line_Y[candidate_line_i]]
+            Lf = np.linalg.norm(gaze_line[-1, :] - gaze_line[0, :])
+            Ll = np.linalg.norm(text_line[-1, :] - text_line[0, :])
+            line_scores[candidate_i] = abs(Ll - Lf)
+        line_i = nearest_line_I[np.argmin(line_scores)]
+        fixation_XY[start_of_line:end_of_line, 1] = line_Y[line_i]
+        start_of_line = end_of_line
+    return fixation_XY
+
+
+######################################################################
 # MERGE
 #
 # Špakov, O., Istance, H., Hyrskykari, A., Siirtola, H., & Räihä,
