6aea66c5a235741bf6152016acc956bc0a248f15
Chandrachud M Gowda <35567444+Chandrachud-2003@users.noreply.github.com>
2023-04-23 16:42:44 -0400

    updted synthetic_data notebook

--- correction.py
+++ correction.py
@@ -186,9 +186,169 @@ def error_noise(y_noise_probability, y_noise, duration_noise, fixations):
     
     return results
 
-# shift
+# slope
+def error_slope(slope, fixations):
+    '''adds a linear slope to the y values of the fixations'''
+    results = [] # creates an empty list results to store the results
+    
+    for fix in fixations: # loop through the list of fixations
+        x, y, duration = fix[0], fix[1], fix[2] # unpack the values from each fixation
+        y_error = int(x * slope) # calculate the slope error for the y value of the fixation
+        results.append([x, y + y_error, duration]) # add the calculated error to the y value and append the result to the `results` list
+    
+    return results # return the final list of results
 
+    # The slope distortion error is described as a systematic measurement error, meaning that it affects 
+    # all fixations in the same way, regardless of the individual fixations. In other words, the slope 
+    # distortion error applies to all fixations in the same way, with a fixed magnitude specified by the 
+    # parameter dslope. On the other hand, the noise error, within-line regression error, and between-line 
+    # regression error are all modeled as random errors, and the magnitude of each 
+    # error is determined by a probability parameter. In the case of slope distortion, since the error
+    # affects all fixations in the same way, there is no need for a probability parameter.
+    
+# shift error
+def error_shift(shift, fixations, aoi_lines):
+    '''adds a constant shift to the y values of the fixations'''
 
+    results = [] # creates an empty list results to store the results
+
+    # Keep track of the line number
+    line_number = 0
+
+    # Keep track of the shift error
+    y_error = 0
+    
+    for fix in fixations: # loop through the list of fixations
+        x, y, duration = fix[0], fix[1], fix[2] # unpack the values from each fixation
+
+        # If the fixation is on the first line, do not apply the shift distortion error
+        if line_number == 0:
+            results.append([x, y, duration])
+        else:
+            results.append([x, y + y_error, duration]) # add the calculated error to the y value and append the result to the `results` list
+
+        # Use the aoi_lines to determine when to increment the line number
+        if y > aoi_lines.at[line_number, 'y'] + aoi_lines.at[line_number, 'height']:
+            line_number += 1
+            # For every pixel that the fixation is below the bottom of the line, increment the shift by shift pixels
+            # Does x and y represent pixels?
+            y_error += int((y - aoi_lines.at[line_number, 'y']) * shift)
+
+    
+    return results # return the final list of results
+
+# within line regression
+def error_within_line_regression(regression_probability, fixations, words_sorted, aoi_lines, duplicate_fixations = True):
+    '''creates a within-line regression error by adding additional fixations to the current line'''
+    
+    results = [] # creates an empty list `results` to store the results
+
+    # Creating a new fixation list that will be used to store all the additional fixations
+    new_fixations = []
+
+    # List to store the current line number
+    line_number = 0
+    
+    for fix in fixations: # loop through the list of fixations
+        x, y, duration = fix[0], fix[1], fix[2] # unpack the values from each fixation
+        results.append([x, y, duration]) # append the current fixation to the `results` list
+        if duplicate_fixations:
+            new_fixations.append([x, y, duration])
+
+        # Use the aoi_lines to determine when to increment the line number
+        if y > aoi_lines.at[line_number, 'y'] + aoi_lines.at[line_number, 'height']:
+            line_number += 1
+        
+        if random.random() < regression_probability: # if a random number is less than the `regression_probability`, add a regression fixation
+            # While loop to get random words until the word is on the current line
+            reg_x = 0
+            reg_y = 0
+
+            # Getting an array with all the words on the current line
+            words_on_line = words_sorted[line_number]
+
+            # Finding the sliced array with words upto the mst recent fixation
+            recent_index = 0
+            for i in range(1, len(words_on_line)):
+                if words_on_line[i][0] > x:
+                    recent_index = i
+                    break
+
+            if recent_index > 0:
+
+                weights = [1/(i+1) for i in range(recent_index)]
+
+                # Getting a random word from the sliced array such that the most recent words have a higher probability of being selected
+                random_word = random.choices(words_on_line[:recent_index], weights=weights, k=1)[0]
+
+                # Getting the x and y coordinates of the random word
+                reg_x = random_word[0]
+                reg_y = random_word[1]
+
+                # Add the regression fixation to the results
+                results.append([reg_x, reg_y, duration])
+
+                if duplicate_fixations:
+                    # Adding an extra correct fixation to the fixation list
+                    new_fixations.append([x, y, duration])
+
+    if duplicate_fixations:
+        return results, new_fixations
+
+    return results # return the final list of results
+
+# between line regression
+def error_between_line_regression(regression_probability, fixations, words_sorted, aoi_lines, duplicate_fixations = True):
+    '''creates a within-line regression error by adding additional fixations to the current line'''
+    
+    results = [] # creates an empty list `results` to store the results
+
+    # Creating a new fixation list that will be used to store all the additional fixations
+    new_fixations = []
+
+    # List to store the current line number
+    line_number = 0
+    
+    for fix in fixations: # loop through the list of fixations
+        x, y, duration = fix[0], fix[1], fix[2] # unpack the values from each fixation
+        results.append([x, y, duration]) # append the current fixation to the `results` list
+        if duplicate_fixations:
+            new_fixations.append([x, y, duration])
+
+        # Use the aoi_lines to determine when to increment the line number
+        if y > aoi_lines.at[line_number, 'y'] + aoi_lines.at[line_number, 'height']:
+            line_number += 1
+        
+        if random.random() < regression_probability and line_number > 0: # if a random number is less than the `regression_probability`, add a regression fixation
+            # While loop to get random words until the word is on the current line
+            reg_x = 0
+            reg_y = 0
+
+            # Getting a random line number lesser than the current line number  with more recent lines linearly more probable than less recent lines
+            weights = [1 / (i+1) for i in range(line_number)]
+            random_line_number = random.choices(range(line_number), weights=weights, k=1)[0] 
+
+            # Getting an array with all the words on the current line
+            words_on_line = words_sorted[random_line_number]
+
+            # Getting a random word from the line with no consideration for the most recent fixation or probability
+            if len(words_on_line) > 0:
+                random_word = random.choices(words_on_line, k=1)[0]
+
+                # Getting the x and y coordinates of the random word
+                reg_x = random_word[0]
+                reg_y = random_word[1]
+
+                # Add the regression fixation to the results
+                results.append([reg_x, reg_y, duration])
+
+                if duplicate_fixations:
+                    # Adding an extra correct fixation to the fixation list
+                    new_fixations.append([x, y, duration])
+
+    if duplicate_fixations:
+        return results, new_fixations
+    return results # return the final list of results
 
 # droop
 
